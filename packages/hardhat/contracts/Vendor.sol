5808c64aa69ad52ab2794a06bb2d7868bfb2d013
Nicholas Wang <clone.of.snake@gmail.com>
2024-09-11 15:30:00 +0200

    Complete buyToken function

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,20 +1,25 @@
 pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
-  // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+contract Vendor is Ownable{
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
-  YourToken public yourToken;
-
+  YourToken public gldTokenContract;
+  uint256 public constant tokensPerEth = 100;
+  
   constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
+    gldTokenContract = YourToken(tokenAddress);
   }
 
   // ToDo: create a payable buyTokens() function:
-
+  function buyTokens() public payable {
+    uint256 amountOfTokens = msg.value * tokensPerEth;
+    require(gldTokenContract.transfer(msg.sender, amountOfTokens), "Transfer failed");
+    emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+  }
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
 
   // ToDo: create a sellTokens(uint256 _amount) function:
