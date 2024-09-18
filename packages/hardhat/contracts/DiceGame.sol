8e0bccb38a41650a1e86d8fc592162e30b4b71f8
Nicholas Wang <clone.of.snake@gmail.com>
2024-09-18 12:00:48 +0200

    CP1. Added console log to see prevHash, nonce, address.

--- packages/hardhat/contracts/DiceGame.sol
+++ packages/hardhat/contracts/DiceGame.sol
@@ -4,49 +4,64 @@ pragma solidity >=0.8.0 <0.9.0; //Do not change the solidity version as it negat
 import "hardhat/console.sol";
 
 contract DiceGame {
-  uint256 public nonce = 0;
-  uint256 public prize = 0;
+  // State variables
+  uint256 public nonce = 0;  // Nonce of the contract, used in rollTheDice() to add randomness
+  uint256 public prize = 0;  // The current prize amount
 
+  // Custom error for insufficient ether sent
   error NotEnoughEther();
 
+  // Events to log important actions
   event Roll(address indexed player, uint256 amount, uint256 roll);
   event Winner(address winner, uint256 amount);
 
+  // Constructor function, called when deploying the contract
   constructor() payable {
     resetPrize();
   }
 
+  // Private function to reset prize amount when someone wins
   function resetPrize() private {
-    prize = ((address(this).balance * 10) / 100);
+    prize = ((address(this).balance * 10) / 100);  // 10% of the contract's balance
   }
 
+  // User call this main function to play the dice game
   function rollTheDice() public payable {
+    // Check if the player sent enough ether to play
     if (msg.value < 0.002 ether) {
       revert NotEnoughEther();
     }
 
-    bytes32 prevHash = blockhash(block.number - 1);
-    bytes32 hash = keccak256(abi.encodePacked(prevHash, address(this), nonce));
-    uint256 roll = uint256(hash) % 16;
+    // Generate a pseudo-random number for the dice roll
+    bytes32 prevHash = blockhash(block.number - 1);  // Get the hash of the previous block
+    bytes32 hash = keccak256(abi.encodePacked(prevHash, address(this), nonce));  // Create a unique hash
+    uint256 roll = uint256(hash) % 16;  // Convert hash to a number between 0 and 15
 
+    console.log("\t", "   Previous Block Hash:", uint256(prevHash)); // To write the RiggedRoll attack contract, we need to know the previous block hash
+    console.log("\t", "   Nonce:", nonce);  // and the rest of the variables that are hashed together
+    console.log("\t", "   Contract Address:", address(this));
     console.log("\t", "   Dice Game Roll:", roll);
 
-    nonce++;
-    prize += ((msg.value * 40) / 100);
+    nonce++;  // Increment the nonce for future rolls
+    prize += ((msg.value * 40) / 100);  // Add 40% of the bet to the prize pool
 
+    // Emit an event with roll details
     emit Roll(msg.sender, msg.value, roll);
 
+    // If the roll is greater than 5, the player loses
     if (roll > 5) {
       return;
     }
 
+    // Player wins if the code reaches here
     uint256 amount = prize;
-    (bool sent, ) = msg.sender.call{value: amount}("");
+    (bool sent, ) = msg.sender.call{value: amount}("");  // Send the prize to the winner
     require(sent, "Failed to send Ether");
 
-    resetPrize();
-    emit Winner(msg.sender, amount);
+    resetPrize();  // Reset the prize for the next game
+    emit Winner(msg.sender, amount);  // Emit a Winner event
   }
 
+  // Function to receive Ether. msg.data must be empty
   receive() external payable {}
 }
--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -4,6 +4,192 @@
  */
 import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 
-const deployedContracts = {} as const;
+const deployedContracts = {
+  31337: {
+    DiceGame: {
+      address: "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e",
+      abi: [
+        {
+          inputs: [],
+          stateMutability: "payable",
+          type: "constructor",
+        },
+        {
+          inputs: [],
+          name: "NotEnoughEther",
+          type: "error",
+        },
+        {
+          anonymous: false,
+          inputs: [
+            {
+              indexed: true,
+              internalType: "address",
+              name: "player",
+              type: "address",
+            },
+            {
+              indexed: false,
+              internalType: "uint256",
+              name: "amount",
+              type: "uint256",
+            },
+            {
+              indexed: false,
+              internalType: "uint256",
+              name: "roll",
+              type: "uint256",
+            },
+          ],
+          name: "Roll",
+          type: "event",
+        },
+        {
+          anonymous: false,
+          inputs: [
+            {
+              indexed: false,
+              internalType: "address",
+              name: "winner",
+              type: "address",
+            },
+            {
+              indexed: false,
+              internalType: "uint256",
+              name: "amount",
+              type: "uint256",
+            },
+          ],
+          name: "Winner",
+          type: "event",
+        },
+        {
+          inputs: [],
+          name: "nonce",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "prize",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "rollTheDice",
+          outputs: [],
+          stateMutability: "payable",
+          type: "function",
+        },
+        {
+          stateMutability: "payable",
+          type: "receive",
+        },
+      ],
+      inheritedFunctions: {},
+    },
+    RiggedRoll: {
+      address: "0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0",
+      abi: [
+        {
+          inputs: [
+            {
+              internalType: "address payable",
+              name: "diceGameAddress",
+              type: "address",
+            },
+          ],
+          stateMutability: "nonpayable",
+          type: "constructor",
+        },
+        {
+          anonymous: false,
+          inputs: [
+            {
+              indexed: true,
+              internalType: "address",
+              name: "previousOwner",
+              type: "address",
+            },
+            {
+              indexed: true,
+              internalType: "address",
+              name: "newOwner",
+              type: "address",
+            },
+          ],
+          name: "OwnershipTransferred",
+          type: "event",
+        },
+        {
+          inputs: [],
+          name: "diceGame",
+          outputs: [
+            {
+              internalType: "contract DiceGame",
+              name: "",
+              type: "address",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "owner",
+          outputs: [
+            {
+              internalType: "address",
+              name: "",
+              type: "address",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "renounceOwnership",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "address",
+              name: "newOwner",
+              type: "address",
+            },
+          ],
+          name: "transferOwnership",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ],
+      inheritedFunctions: {
+        owner: "@openzeppelin/contracts/access/Ownable.sol",
+        renounceOwnership: "@openzeppelin/contracts/access/Ownable.sol",
+        transferOwnership: "@openzeppelin/contracts/access/Ownable.sol",
+      },
+    },
+  },
+} as const;
 
 export default deployedContracts satisfies GenericContractsDeclaration;
